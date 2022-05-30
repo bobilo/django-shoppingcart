200ee96144193a9ba25e3638bb1df007a6578b3e
Bonface Obilo <bonfaceobilo@gmail.com>
2022-05-30 12:35:41 +0300

    group item variations

--- store/views.py
+++ store/views.py
@@ -49,6 +49,8 @@ def product_detail(request, category_slug, product_slug):
 
 def search(request):
     products = None
+    product_count = 0
+
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
